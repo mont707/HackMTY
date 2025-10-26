233cd066cd98a1bffd3420a3542562d661d7e847
ferrc897 <ferrodriguez8972005@gmail.com>
2025-10-25 18:14:30 -0600

    minor bugs fixed

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import os
+app = Flask(
+    __name__,
+    template_folder=os.path.join("..", "Web", "templates"),
+    static_folder=os.path.join("..","Web", "static")
+    )
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
